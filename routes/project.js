9f5abd1d107db06b8edb59f4482b5116c7500f96
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-12-08 13:24:33 -0500

    added route endpoints to get list of projects added route endpoint to get a project by it's id added route endpoint to post a new project

--- routes/project.js
+++ routes/project.js
@@ -17,4 +17,46 @@ router.get('/', (req, res) => {
     });
 });
 
+// GET /api/projects/:id
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  projectsDb
+    .get(id)
+    .then(project => {
+      res
+        .send(project);
+    })
+    .catch(err => {
+      res
+        .json({ message: 'unable to get projects' });
+    });
+});
+
+// POST /api/projects/create
+router.post('/create', (req, res) => {
+  const project = req.body;
+  if (project.name && project.description) {
+    projectsDb
+      .insert(project)
+      .then(idInfo => {
+        projectsDb
+          .get(idInfo.id)
+          .then(project => {
+            res
+              .status(201)
+              .json(idInfo);
+          });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ message: 'failed to insert projcet into db' });
+      })
+  } else {
+    res
+    .status(400)
+    .json({ errorMessage: 'please provide a name and description for the project' });
+  }
+});
+
 module.exports = router;
