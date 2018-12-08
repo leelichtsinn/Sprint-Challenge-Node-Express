9d23f5029f15b44ba57b089fe6017be5fe8e160c
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2018-12-08 14:52:05 -0500

    added route endpoint to insert new actions for a project

--- routes/actions.js
+++ routes/actions.js
@@ -33,5 +33,36 @@ router.get('/:id', (req, res) => {
     })
 });
 
+// POST /api/actions/create
+router.post('/create', (req, res) => {
+  const action = req.body;
+  if (action.project_id && action.description && action.notes) {
+    actionsDb
+      .insert(action)
+      .then(idInfo => {
+        actionsDb
+          .get(idInfo.id)
+          .then(acton => {
+            res
+              .status(201)
+              .json(idInfo);
+          });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ message: 'failed to insert action into db '});
+      });
+  } else {
+    res
+      .status(400)
+      .json({ errorMessage: 'please provide a description and notes for the action' });
+  }
+})
+
+// PUT /api/actions/update/:id
+
+// DELETE /api actions/delete/:id
+
 
 module.exports = router;
